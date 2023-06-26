1805c0557c980747262a37b4f8c1af0db091bdeb
bharat <bharatkumarchuahan179@gmail.com>
2023-06-26 21:27:28 +0530

    Recipe Controller (Fix): Cross origin added.

--- src/main/java/com/example/Recipe/Controller/RecipeController.java
+++ src/main/java/com/example/Recipe/Controller/RecipeController.java
@@ -10,11 +10,13 @@ import com.example.Recipe.model.Instruction;
 import com.example.Recipe.model.Nutrition;
 import com.example.Recipe.model.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RecipeController {
 
 
